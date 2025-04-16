ddec5faca6f6eff81d65ad82aa240906da71700f
Rakesh Baruah <rbaruah@crisisprevention.com>
2025-04-16 10:30:44 -0500

    run the application to view the reviews to be analyzed

--- Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
+++ Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684c46b47875a5b80dcbb9be75d828643338a130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b5f4f0d63af9219a58ed8189e6d38130471f02")]
 [assembly: System.Reflection.AssemblyProductAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

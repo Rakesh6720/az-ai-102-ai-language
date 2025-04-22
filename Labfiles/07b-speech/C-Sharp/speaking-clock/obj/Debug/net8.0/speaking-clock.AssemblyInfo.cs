37ca8369515ee743d3758e0199bc16ddfea7b4fb
Rakesh Baruah <rbaruah@crisisprevention.com>
2025-04-22 12:10:02 -0500

    update classify-text program.cs

--- Labfiles/07b-speech/C-Sharp/speaking-clock/obj/Debug/net8.0/speaking-clock.AssemblyInfo.cs
+++ Labfiles/07b-speech/C-Sharp/speaking-clock/obj/Debug/net8.0/speaking-clock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("speaking-clock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6ed23a5b816c4e5b8c76458dd12dfa97cb3fa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9512b86f0e252dd59df1d15ba4af8c7c1f27a071")]
 [assembly: System.Reflection.AssemblyProductAttribute("speaking-clock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("speaking-clock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

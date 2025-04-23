239ffe6284e55b93cd4ffba6844159e01c6159fd
Rakesh Baruah <rbaruah@crisisprevention.com>
2025-04-23 14:03:34 -0500

    Add Azure.AI.Translation.Text package and update project dependencies

--- Labfiles/06-translate-text/C-Sharp/text-translation/obj/Debug/net7.0/translate_text.AssemblyInfo.cs
+++ Labfiles/06-translate-text/C-Sharp/text-translation/obj/Debug/net7.0/translate_text.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9512b86f0e252dd59df1d15ba4af8c7c1f27a071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e895e6f0bf7b5cbf32ed3919c978c8e979c7d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyTitleAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

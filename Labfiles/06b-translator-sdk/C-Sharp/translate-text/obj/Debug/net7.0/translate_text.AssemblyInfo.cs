0d0a951e810ae85c6ace610032cc1289b98e28ce
Rakesh Baruah <rbaruah@crisisprevention.com>
2025-04-23 14:10:19 -0500

    Add Azure.AI.Translation.Text package and implement translation functionality

--- Labfiles/06b-translator-sdk/C-Sharp/translate-text/obj/Debug/net7.0/translate_text.AssemblyInfo.cs
+++ Labfiles/06b-translator-sdk/C-Sharp/translate-text/obj/Debug/net7.0/translate_text.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9512b86f0e252dd59df1d15ba4af8c7c1f27a071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+239ffe6284e55b93cd4ffba6844159e01c6159fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyTitleAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
